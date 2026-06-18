890dccf64d2134889576d6b8db8e62ffe0a6f7c7
Roaunak Gupta <roaunakgupta@gmail.com>
2026-06-18 18:35:06 +0530

    Learn about Time and Space Complexity,their Types, Intoduction of array from Numpy

---
+++ Learning/19.0. Time&Space_Complexity.py
@@ -0,0 +1,19 @@
+
+#! Time and Space Complexity 
+
+    #* Time Complexity - 
+        # Time Complexity measures how the running time of an algorithm grows as the input size increases.
+    
+    #* Space Complexity - 
+        # Space Complexity measures how much extra memory an algorithm needs.
+
+    #* Types of Analysis -
+        # 1. Best Case - (Ω) - Omega Notation
+        # 2. Avarage Case - (ʘ) - Theta Notation
+        # 3. Worth Case - (O) - Big-O Notation
+
+    #* Types of Time Complexity -
+        # 1. Constant Time - O(1)
+        # 2. Linear Time - O(n)
+        # 3. Logarithum Time - O(log(n))
+        # 4. Quadratic Time - O(2²)
