136e3f9f811dea41fc620d61116d056b78dd7f72
Roaunak Gupta <roaunakgupta@gmail.com>
2026-06-21 23:54:50 +0530

    Implement insert_on_start, insert_on_end, insert_on_mid and Delete Node Functions

--- Learning/19.2. LinkedList.py
+++ Learning/19.2. LinkedList.py
@@ -4,46 +4,11 @@ class Node:
         self.next = None
 
 
-#! Example - 1 : Create Nodes using only Node Class
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-node4 = Node(40)
-
-
-# Link Nodes
-node1.next = node2
-node2.next = node3
-
-# Traverse and print
-current = node1
-
-while current:
-    print(current.data)
-    current = current.next
-
-# ------------------------------------------------------------
-#! Example - 2 : LinkedList
-
-
 class LinkedList:
+
     def __init__(self):
         self.head = None
-
-    def append(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-            print(f"New Node : {new_node}")
-            return
-
-        current = self.head
-
-        while current.next:
-            current = current.next
-
-        current.next = new_node
+        print("New LinkedList is Created as Succesfully...")
 
     def display(self):
         current = self.head
@@ -53,11 +18,85 @@ class LinkedList:
             current = current.next
         print("None")
 
+    def insert_on_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            print(f"First Node Element : {new_node.data}")
+            return
+
+        current = self.head
+
+        while current.next:
+            current = current.next
+
+        current.next = new_node
+
+    def insert_on_start(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            print(f"First Node Element : {new_node.data}")
+            return
+
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_on_mid(self, data, location):
+
+        new_node = Node(data)
+        current = self.head
+        while current:
+            if current.data == location:
+                temp = current.next
+                current.next = new_node
+                new_node.next = temp
+                print(f"Node {new_node.data} Inserted after : {current.data}")
+                break
+            current = current.next
+
+    def delete_node(self, value):
+
+        head = self.head
+        current = self.head
+        previous_node = self.head
+        while current:
+            if current.data == int(value):
+                if current == head:
+                    self.head = current.next
+                    print(f"Head Element {head.data} Deleted.")
+                    del current
+                else:
+                    if current.next == None:
+                        while previous_node:
+                            if previous_node.next == current:
+                                previous_node.next = None
+                                del current
+                                print(f"Last Element Deleted.")
+                            previous_node = previous_node.next
+                    else:
+                        while previous_node:
+                            if previous_node.next == current:
+                                previous_node.next = current.next
+                                print(f"Mid Element {current.data} Deleted.")
+                                del current
+                                break
+                            previous_node = previous_node.next
+                break
+            current = current.next
+
 
 ll = LinkedList()
-ll.append(10)
-ll.append(20)
-ll.append(30)
-ll.append(40)
-
+ll.insert_on_end(10)
+ll.insert_on_end(20)
+ll.insert_on_end(30)
+ll.display()
+ll.insert_on_mid(15, 10)
+ll.display()
+ll.insert_on_end(40)
+ll.insert_on_end(50)
+ll.display()
+ll.insert_on_start(5)
+ll.delete_node(5)
 ll.display()
--- Learning/tempCodeRunnerFile.py
+++ Learning/tempCodeRunnerFile.py
@@ -1,15 +1,1 @@
-def merge_short(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    print(f"Mid {mid}")
-    left = merge_short(arr[:mid])
-    right = merge_short(arr[mid:])
-    print(f"{left} and {right}")
-    return sorted(left + right)
-
-
-li = [5,8,1,4,2,6,5,9,6,3,6,2,5,8]
-result = merge_short(li)
-print(result)
+current
