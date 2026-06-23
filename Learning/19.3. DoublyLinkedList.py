824b1caba67bebb1654c3e6ffc9578d1d9f6fd5a
Roaunak Gupta <roaunakgupta@gmail.com>
2026-06-23 12:52:40 +0530

    Implement Doubly LinkedList - Insert on Head, Insert on Teal, Display Forword and Display Backword Functions

---
+++ Learning/19.3. DoublyLinkedList.py
@@ -0,0 +1,95 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.previous = None
+        self.next = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.teal = None
+        print("New Doubly Linked List is Created as Succesfully...")
+
+    def display_forword(self):
+        current = self.head
+
+        print("Head", end=" -> ")
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("Teal")
+
+    def display_backword(self):
+        current = self.teal
+        print("Teal", end=" <- ")
+        while current:
+            print(current.data, end=" <- ")
+            current = current.previous
+        print("Head")
+
+    def insert_on_teal(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            print(f"First Node Created.")
+            return
+        current = self.head
+
+        while current.next:
+            current = current.next
+
+        current.next = new_node
+        new_node.previous = current
+        self.teal = current
+
+    def insert_on_head(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            print(f"First Node Created with the data : {new_node.data}")
+            return
+
+        temp = self.head
+
+        new_node.next = self.head
+        temp.previous = new_node
+        self.head = new_node
+
+    def insert_on_mid(self, data, location):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            print(f"First Node Created with the data : {new_node.data}")
+            return
+        
+        current = self.head
+        
+        # while current:
+        #     if current.data == int(location):
+        #         temp = current
+        #         new_node.next = current.next
+        #         new_node.previous = current
+        #         temp = current.next
+        #         temp.previous = new_node
+        #         current.next = new_node
+        #         break
+        #     current = current.next
+
+
+dll = DoublyLinkedList()
+dll.insert_on_teal(2)
+dll.insert_on_teal(4)
+dll.insert_on_teal(6)
+dll.insert_on_teal(8)
+dll.insert_on_teal(10)
+dll.insert_on_teal(12)
+dll.insert_on_teal(14)
+dll.insert_on_head(1)
+dll.insert_on_teal(16)
+dll.insert_on_teal(18)
+# dll.insert_on_mid(17,18)
+dll.insert_on_teal(20)
+dll.display_forword()
+dll.display_backword()
--- Learning/tempCodeRunnerFile.py
+++ Learning/tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-current
+le current:
+        #     if current.data == int(location):
+        #         temp = current
+        #         new_node.next = current.next
+        #         new_node.previous = current
+        #         temp = current.next
+        #         temp.previous = new_node
+        #         current.next = new_node
+        #         break
+        #     
